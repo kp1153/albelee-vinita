c4c904c006fe5bac508646f255af49ff834021ef
kp1153 <kamta_tiwari@yahoo.com>
2026-03-14 21:54:48 +0530

    fix upload preset env var

--- app/api/admin/upload/route.js
+++ app/api/admin/upload/route.js
@@ -4,7 +4,7 @@ export async function POST(req) {
 
   const cloudFormData = new FormData();
   cloudFormData.append('file', file);
-  cloudFormData.append('upload_preset', process.env.CLOUDINARY_UPLOAD_PRESET);
+  cloudFormData.append('upload_preset', process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET);
 
   const res = await fetch(
     `https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload`,
